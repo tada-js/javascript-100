85c9df48943d9983cb5c4d3e77e050abf4140c90
Kim Seong Hun <justfix010@gmail.com>
2022-08-15 19:46:18 +0900

    :memo: Add : 문제6 False
    문제6 False 추가

---
+++ 01-50/06.js
@@ -0,0 +1,7 @@
+console.log("NaN : " + Boolean(NaN))
+console.log("1 : " + Boolean(1))
+console.log(" : " + Boolean(""))
+console.log("0 : " + Boolean(0))
+console.log("undefined : " + Boolean(undefined))
+
+// 1 => Truth
