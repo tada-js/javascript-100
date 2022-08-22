8237a1c018818eae58ba05c5e061f38652fb157e
Kim Seong Hun <justfix010@gmail.com>
2022-08-23 00:04:53 +0900

    Add : 문제16 로꾸거

---
+++ 01-50/16_case3.js
@@ -0,0 +1,9 @@
+const n = prompt("입력하세요.");
+
+const reverseString = n.split("").reverse().join("");
+/*
+ * split() 메서드는 문자열을 배열로 만들어 반환하고,
+ * reverse() 메서드는 배열의 순서를 반전하며,
+ * join() 메서드는 원소를 모두 붙여 문자열로 반환합니다.
+ */
+console.log(reverseString);
